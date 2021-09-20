bb0577c636b92b521d0bfee8d43b9349be662173
daemon1024 <barun1024@gmail.com>
2021-09-20 11:45:28 +0530

    Add Version Command
    Displays version info of the cli and running kubearmor image
    
    Signed-off-by: Barun Acharya <barun.acharya@accuknox.com>

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
 CURDIR=$(shell pwd)
+VERSION=$(shell git describe --tags --dirty --always)
 
 .PHONY: build
 build:
-	cd $(CURDIR); go mod tidy; go build -o karmor
+	cd $(CURDIR)
+	go mod tidy
+	CGO_ENABLED=0 go build \
+	-ldflags "-w -s -X github.com/kubearmor/kubearmor-client/version.version=${VERSION}" \
+	-o karmor
---
+++ version/version.go
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2021 Authors of KubeArmor
+
+package version
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+
+	"github.com/kubearmor/kubearmor-client/k8s"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var version string
+
+func PrintVersion(c *k8s.Client) error {
+	fmt.Printf("karmor version %s %s/%s\n", version, runtime.GOOS, runtime.GOARCH)
+	kubearmorVersion, err := getKubeArmorVersion(c)
+	if err != nil {
+		return nil
+	}
+	if kubearmorVersion == "" {
+		fmt.Printf("kubearmor not running\n")
+		return nil
+	}
+	fmt.Printf("kubearmor image (running) version %s\n", kubearmorVersion)
+	return nil
+}
+
+func getKubeArmorVersion(c *k8s.Client) (string, error) {
+	pods, err := c.K8sClientset.CoreV1().Pods("kube-system").List(context.Background(), metav1.ListOptions{LabelSelector: "kubearmor-app=kubearmor"})
+	if err != nil {
+		return "", err
+	}
+	if len(pods.Items) > 0 {
+		image := pods.Items[0].Spec.Containers[0].Image
+		return image, nil
+	}
+	return "", nil
+}
