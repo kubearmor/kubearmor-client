08d95f2d70dc66b800a72898742d3cd01e8f0cb7
Eswar Rajan Subramanian <eswar@accuknox.com>
2021-12-15 14:50:33 +0530

    Modifying/Adding support for karmor to support non-k8s control plane
    Signed-off-by: Eswar Rajan Subramanian <eswar@accuknox.com>

--- cmd/vm.go
+++ cmd/vm.go
@@ -10,17 +10,97 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var vmOptions vm.Options
+var (
+	scriptOptions vm.ScriptOptions
+	policyOptions vm.PolicyOptions
+)
 
 // vmCmd represents the vm command
 var vmCmd = &cobra.Command{
 	Use:   "vm",
-	Short: "Download VM install script from kvmservice",
-	Long:  `Download VM install script from kvmservice`,
+	Short: "VM commands for kvmservice",
+	Long:  `VM commands for kvmservice`,
+}
+
+// vmPolicyCmd represents the vm command for policy enforcement
+var vmPolicyCmd = &cobra.Command{
+	Use:   "policy",
+	Short: "policy handling for vm nonk8s control plane",
+	Long:  `policy handling for vm nonk8s control plane`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if err := vm.FileDownload(client, vmOptions); err != nil {
+		return nil
+	},
+}
+
+// vmPolicyAddCmd represents the vm add policy command for policy enforcement
+var vmPolicyAddCmd = &cobra.Command{
+	Use:   "add",
+	Short: "add policy for vm k8s/nonk8s control plane",
+	Long:  `add policy for vm k8s/nonk8s control plane`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
+}
+
+// vmPolicyupdateCmd represents the vm update policy command for policy enforcement
+var vmPolicyupdateCmd = &cobra.Command{
+	Use:   "update",
+	Short: "update policy for vm k8s/nonk8s control plane",
+	Long:  `update policy for vm k8s/nonk8s control plane`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
+}
+
+// vmPolicyDeleteCmd represents the vm delete policy command for policy enforcement
+var vmPolicyDeleteCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "delete policy for vm k8s/nonk8s control plane",
+	Long:  `delete policy for vm k8s/nonk8s control plane`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
+}
+
+// vmAddCmd represents the vm command for vm onboarding
+var vmAddCmd = &cobra.Command{
+	Use:   "add",
+	Short: "add/onboard a new vm for nonk8s control plane",
+	Long:  `add/onboard a new vm for nonk8s control plane`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
+}
+
+// vmDelCmd represents the vm command for vm onboarding
+var vmDelCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "delete/offboard a vm from nonk8s control plane",
+	Long:  `delete/offboard a vm from nonk8s control plane`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
+}
+
+// vmScriptCmd represents the vm command for script download
+var vmScriptCmd = &cobra.Command{
+	Use:   "getscript",
+	Short: "download vm installation script for nonk8s control plane",
+	Long:  `download vm installation script for nonk8s control plane`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if err := vm.GetScript(client, scriptOptions); err != nil {
 			return err
 		}
+		return nil
+	},
+}
+
+// vmLabelCmd represents the vm command for script download
+var vmLabelCmd = &cobra.Command{
+	Use:   "label",
+	Short: "manage vm labels for nonk8s control plane",
+	Long:  `manage vm labels for nonk8s control plane`,
+	RunE: func(cmd *cobra.Command, args []string) error {
 		return nil
 	},
 }
@@ -31,13 +111,31 @@ var vmCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(vmCmd)
-	vmCmd.Flags().StringVarP(&vmOptions.Port, "port", "p", "32770", "Port of kvmservice")
-	vmCmd.Flags().StringVarP(&vmOptions.VMName, "kvm", "v", "", "Name of configured vm")
-	vmCmd.Flags().StringVarP(&vmOptions.File, "file", "f", "none", "Filename with path to store the configured vm installation script")
+
+	// All subcommands
+	vmCmd.AddCommand(vmAddCmd)
+	vmCmd.AddCommand(vmDelCmd)
+	vmCmd.AddCommand(vmPolicyCmd)
+	vmCmd.AddCommand(vmScriptCmd)
+	vmCmd.AddCommand(vmLabelCmd)
+
+	// Subcommand for policy command
+	vmPolicyCmd.AddCommand(vmPolicyAddCmd)
+	vmPolicyCmd.AddCommand(vmPolicyupdateCmd)
+	vmPolicyCmd.AddCommand(vmPolicyDeleteCmd)
+
+	// Options for vm script download
+	vmScriptCmd.Flags().StringVarP(&scriptOptions.Port, "port", "p", "32770", "Port of kvmservice")
+	vmScriptCmd.Flags().StringVarP(&scriptOptions.VMName, "kvm", "v", "", "Name of configured vm")
+	vmScriptCmd.Flags().StringVarP(&scriptOptions.File, "file", "f", "none", "Filename with path to store the configured vm installation script")
+
+	// Options for vm policy enforcement
+	vmPolicyCmd.Flags().StringVarP(&policyOptions.File, "file", "f", "none", "Policy file")
 
 	// Marking this flag as markedFlag and mandatory
-	err := vmCmd.MarkFlagRequired("kvm")
+	err := vmScriptCmd.MarkFlagRequired("kvm")
 	if err != nil {
 		_ = fmt.Errorf("kvm option not supplied")
 	}
+
 }
---
+++ vm/policy.go
@@ -0,0 +1,10 @@
+package vm
+
+// Options for karmor vm policy
+type PolicyOptions struct {
+	File string
+}
+
+func PolicyHandling() {
+
+}
