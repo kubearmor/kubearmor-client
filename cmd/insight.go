613399c6486093509decdd0e1a39dbb2f8f2da82
Eswar Rajan Subramanian <eswar@accuknox.com>
2022-04-19 18:28:59 +0530

    Network insight support
    Signed-off-by: Eswar Rajan Subramanian <eswar@accuknox.com>

--- cmd/insight.go
+++ cmd/insight.go
@@ -13,8 +13,8 @@ var insightOptions insight.Options
 // insightCmd represents the insight command
 var insightCmd = &cobra.Command{
 	Use:   "insight",
-	Short: "Observe policy from the discovery engine",
-	Long:  `Observe policy from the discovery engine`,
+	Short: "Policy insight from discovery engine",
+	Long:  `Policy insight from discovery engine`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if err := insight.StartInsight(insightOptions); err != nil {
 			return err
@@ -27,9 +27,12 @@ func init() {
 	rootCmd.AddCommand(insightCmd)
 
 	insightCmd.Flags().StringVar(&insightOptions.GRPC, "gRPC", "", "gRPC server information")
+	insightCmd.Flags().StringVar(&insightOptions.Source, "source", "all", "The DB for insight : system|network")
 	insightCmd.Flags().StringVar(&insightOptions.Labels, "labels", "", "Labels for resources")
 	insightCmd.Flags().StringVar(&insightOptions.Containername, "containername", "", "Filter according to the Container name")
 	insightCmd.Flags().StringVar(&insightOptions.Clustername, "clustername", "", "Filter according to the Cluster name")
 	insightCmd.Flags().StringVar(&insightOptions.Fromsource, "fromsource", "", "Filter according to the source path")
 	insightCmd.Flags().StringVarP(&insightOptions.Namespace, "namespace", "n", "", "Namespace for resources")
+	insightCmd.Flags().StringVar(&insightOptions.Type, "type", "", "NW packet type : ingress|egress")
+	insightCmd.Flags().StringVar(&insightOptions.Rule, "rule", "", "NW packet Rule")
 }
