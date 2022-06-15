cd335e81898390160f6a76a0c745cabda351e639
essietom <essietom@gmail.com>
2022-06-15 16:31:31 +0000

    create probe utility, probe host for observability/audit
    Signed-off-by: essietom <essietom@gmail.com>
    
    squash all commits for karmor probe utility
    
    rename methods properly
    
    Signed-off-by: essietom <essietom@gmail.com>
    
    check supported enforcement for host
    
    Signed-off-by: essietom <essietom@gmail.com>
    
    correct print output
    
    Signed-off-by: essietom <essietom@gmail.com>
    
    rrefactor code to remove redundancy
    
    Signed-off-by: essietom <essietom@gmail.com>
    
    format text output
    
    Signed-off-by: essietom <essietom@gmail.com>
    
    check node observability support
    
    Signed-off-by: essietom <essietom@gmail.com>
    
    refactor code
    
    Signed-off-by: essietom <essietom@gmail.com>
    
    refactor code
    
    Signed-off-by: essietom <essietom@gmail.com>
    
    refactor code
    
    Signed-off-by: essietom <essietom@gmail.com>
    
    refactor code
    
    Signed-off-by: essietom <essietom@gmail.com>
    
    remove non probe commits
    
    Signed-off-by: essietom <essietom@gmail.com>
    
    probe deployment
    
    Signed-off-by: essietom <essietom@gmail.com>
    
    handle error from bold text
    
    Signed-off-by: essietom <essietom@gmail.com>
    
    refactor code, check bpf support in lsm, check lib module in kernel header
    
    Signed-off-by: essietom <essietom@gmail.com>
    
    format code fix indentation
    
    Signed-off-by: essietom <essietom@gmail.com>
    
    add licence identifier
    
    Signed-off-by: essietom <essietom@gmail.com>

---
+++ cmd/probe.go
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2022 Authors of KubeArmor
+
+package cmd
+
+import (
+	"github.com/kubearmor/kubearmor-client/probe"
+	"github.com/spf13/cobra"
+)
+
+var probeInstallOptions probe.ProbeOptions
+
+// probeCmd represents the get command
+var probeCmd = &cobra.Command{
+	Use:   "probe",
+	Short: "Display probe information",
+	Long:  `Display probe information`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if err := probe.PrintProbeResult(client, probeInstallOptions); err != nil {
+			return err
+		}
+		return nil
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(probeCmd)
+	probeCmd.Flags().StringVarP(&probeInstallOptions.Namespace, "namespace", "n", "default", "Namespace for resources")
+	probeCmd.Flags().BoolVar(&probeInstallOptions.Full, "full", false, "Full performs full probing")
+}
