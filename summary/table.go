d368050e0d8ff4675a7fc5260dda6d3ed2b3fd49
Eswar Rajan <89014588+seswarrajan@users.noreply.github.com>
2022-09-06 18:42:42 +0530

    Add filter RequestType for process/file/nw summary (#169)
    Signed-off-by: Eswar Rajan Subramanian <eswar@accuknox.com>

--- summary/table.go
+++ summary/table.go
@@ -21,83 +21,88 @@ var (
 )
 
 // DisplaySummaryOutput function
-func DisplaySummaryOutput(resp *opb.Response, revDNSLookup bool) {
+func DisplaySummaryOutput(resp *opb.Response, revDNSLookup bool, requestType string) {
 
 	podInfo := resp.PodName + "/" + resp.Namespace + "/" + resp.ClusterName + "/" + resp.Label + "/" + resp.ContainerName
 
 	fmt.Printf("\nPodInfo : [%s]\n", podInfo)
 
-	if len(resp.ProcessData) > 0 {
-
-		procRowData := [][]string{}
-		// Display process data
-		fmt.Printf("\nProcess Data\n")
-		for _, procData := range resp.ProcessData {
-			procStrSlice := []string{}
-			procStrSlice = append(procStrSlice, procData.ParentProcName)
-			procStrSlice = append(procStrSlice, procData.ProcName)
-			procStrSlice = append(procStrSlice, procData.Count)
-			procStrSlice = append(procStrSlice, procData.UpdatedTime)
-			procStrSlice = append(procStrSlice, procData.Status)
-			procRowData = append(procRowData, procStrSlice)
+	if strings.Contains(requestType, "process") {
+		if len(resp.ProcessData) > 0 {
+			procRowData := [][]string{}
+			// Display process data
+			fmt.Printf("\nProcess Data\n")
+			for _, procData := range resp.ProcessData {
+				procStrSlice := []string{}
+				procStrSlice = append(procStrSlice, procData.ParentProcName)
+				procStrSlice = append(procStrSlice, procData.ProcName)
+				procStrSlice = append(procStrSlice, procData.Count)
+				procStrSlice = append(procStrSlice, procData.UpdatedTime)
+				procStrSlice = append(procStrSlice, procData.Status)
+				procRowData = append(procRowData, procStrSlice)
+			}
+			WriteTable(SysProcHeader, procRowData)
+			fmt.Printf("\n")
 		}
-		WriteTable(SysProcHeader, procRowData)
-		fmt.Printf("\n")
 	}
 
-	if len(resp.FileData) > 0 {
-		fmt.Printf("\nFile Data\n")
-		// Display file data
-		fileRowData := [][]string{}
-		for _, fileData := range resp.FileData {
-			fileStrSlice := []string{}
-			fileStrSlice = append(fileStrSlice, fileData.ParentProcName)
-			fileStrSlice = append(fileStrSlice, fileData.ProcName)
-			fileStrSlice = append(fileStrSlice, fileData.Count)
-			fileStrSlice = append(fileStrSlice, fileData.UpdatedTime)
-			fileStrSlice = append(fileStrSlice, fileData.Status)
-			fileRowData = append(fileRowData, fileStrSlice)
+	if strings.Contains(requestType, "file") {
+		if len(resp.FileData) > 0 {
+			fmt.Printf("\nFile Data\n")
+			// Display file data
+			fileRowData := [][]string{}
+			for _, fileData := range resp.FileData {
+				fileStrSlice := []string{}
+				fileStrSlice = append(fileStrSlice, fileData.ParentProcName)
+				fileStrSlice = append(fileStrSlice, fileData.ProcName)
+				fileStrSlice = append(fileStrSlice, fileData.Count)
+				fileStrSlice = append(fileStrSlice, fileData.UpdatedTime)
+				fileStrSlice = append(fileStrSlice, fileData.Status)
+				fileRowData = append(fileRowData, fileStrSlice)
+			}
+			WriteTable(SysFileHeader, fileRowData)
+			fmt.Printf("\n")
 		}
-		WriteTable(SysFileHeader, fileRowData)
-		fmt.Printf("\n")
 	}
 
-	if len(resp.InNwData) > 0 {
-		fmt.Printf("\nIncoming Server connection\n")
-		// Display server conn data
-		inNwRowData := [][]string{}
-		for _, inNwData := range resp.InNwData {
-			inNwStrSlice := []string{}
-			domainName := dnsLookup(inNwData.IP, revDNSLookup)
-			inNwStrSlice = append(inNwStrSlice, inNwData.Protocol)
-			inNwStrSlice = append(inNwStrSlice, inNwData.Command)
-			inNwStrSlice = append(inNwStrSlice, domainName)
-			inNwStrSlice = append(inNwStrSlice, inNwData.Port)
-			inNwStrSlice = append(inNwStrSlice, inNwData.Namespace)
-			inNwStrSlice = append(inNwStrSlice, inNwData.Labels)
-			inNwRowData = append(inNwRowData, inNwStrSlice)
+	if strings.Contains(requestType, "network") {
+		if len(resp.InNwData) > 0 {
+			fmt.Printf("\nIncoming Server connection\n")
+			// Display server conn data
+			inNwRowData := [][]string{}
+			for _, inNwData := range resp.InNwData {
+				inNwStrSlice := []string{}
+				domainName := dnsLookup(inNwData.IP, revDNSLookup)
+				inNwStrSlice = append(inNwStrSlice, inNwData.Protocol)
+				inNwStrSlice = append(inNwStrSlice, inNwData.Command)
+				inNwStrSlice = append(inNwStrSlice, domainName)
+				inNwStrSlice = append(inNwStrSlice, inNwData.Port)
+				inNwStrSlice = append(inNwStrSlice, inNwData.Namespace)
+				inNwStrSlice = append(inNwStrSlice, inNwData.Labels)
+				inNwRowData = append(inNwRowData, inNwStrSlice)
+			}
+			WriteTable(SysNwHeader, inNwRowData)
+			fmt.Printf("\n")
 		}
-		WriteTable(SysNwHeader, inNwRowData)
-		fmt.Printf("\n")
-	}
 
-	if len(resp.OutNwData) > 0 {
-		fmt.Printf("\nOutgoing Server connection\n")
-		// Display server conn data
-		outNwRowData := [][]string{}
-		for _, outNwData := range resp.OutNwData {
-			outNwStrSlice := []string{}
-			domainName := dnsLookup(outNwData.IP, revDNSLookup)
-			outNwStrSlice = append(outNwStrSlice, outNwData.Protocol)
-			outNwStrSlice = append(outNwStrSlice, outNwData.Command)
-			outNwStrSlice = append(outNwStrSlice, domainName)
-			outNwStrSlice = append(outNwStrSlice, outNwData.Port)
-			outNwStrSlice = append(outNwStrSlice, outNwData.Namespace)
-			outNwStrSlice = append(outNwStrSlice, outNwData.Labels)
-			outNwRowData = append(outNwRowData, outNwStrSlice)
+		if len(resp.OutNwData) > 0 {
+			fmt.Printf("\nOutgoing Server connection\n")
+			// Display server conn data
+			outNwRowData := [][]string{}
+			for _, outNwData := range resp.OutNwData {
+				outNwStrSlice := []string{}
+				domainName := dnsLookup(outNwData.IP, revDNSLookup)
+				outNwStrSlice = append(outNwStrSlice, outNwData.Protocol)
+				outNwStrSlice = append(outNwStrSlice, outNwData.Command)
+				outNwStrSlice = append(outNwStrSlice, domainName)
+				outNwStrSlice = append(outNwStrSlice, outNwData.Port)
+				outNwStrSlice = append(outNwStrSlice, outNwData.Namespace)
+				outNwStrSlice = append(outNwStrSlice, outNwData.Labels)
+				outNwRowData = append(outNwRowData, outNwStrSlice)
+			}
+			WriteTable(SysNwHeader, outNwRowData)
+			fmt.Printf("\n")
 		}
-		WriteTable(SysNwHeader, outNwRowData)
-		fmt.Printf("\n")
 	}
 }
 
