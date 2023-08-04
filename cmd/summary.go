4eece9161466a5b2d7b6d63ae3de16d7be3c90c6
PrimalPimmy <Prashant20.pm@gmail.com>
2023-08-04 21:24:26 +0530

    fixed accidently changing podname to containername
    Signed-off-by: PrimalPimmy <Prashant20.pm@gmail.com>

--- cmd/summary.go
+++ cmd/summary.go
@@ -29,7 +29,7 @@ func init() {
 	summaryCmd.Flags().StringVar(&summaryOptions.GRPC, "gRPC", "", "gRPC server information")
 	summaryCmd.Flags().StringVarP(&summaryOptions.Labels, "labels", "l", "", "Labels")
 	summaryCmd.Flags().StringVarP(&summaryOptions.Namespace, "namespace", "n", "", "Namespace")
-	summaryCmd.Flags().StringVarP(&summaryOptions.PodName, "pod", "p", "", "ContainerName")
+	summaryCmd.Flags().StringVarP(&summaryOptions.PodName, "pod", "p", "", "PodName")
 	summaryCmd.Flags().StringVarP(&summaryOptions.Type, "type", "t", summary.DefaultReqType, "Summary filter type : process|file|network ")
 	summaryCmd.Flags().StringVar(&summaryOptions.ClusterName, "cluster", "", "Cluster name")
 	summaryCmd.Flags().StringVar(&summaryOptions.ContainerName, "container", "", "Container name")
