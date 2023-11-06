564702cd212b5ff444cc425f766b97ee42796e6b
Ankur Kothiwal <ankur.kothiwal99@gmail.com>
2023-11-06 10:09:43 +0530

    fix `karmor uninstall`
    Signed-off-by: Ankur Kothiwal <ankur.kothiwal99@gmail.com>
    Signed-off-by: Harisudarsan <sudarshanhari561@gmail.com>

--- install/defaults.go
+++ install/defaults.go
@@ -6,7 +6,23 @@ package install
 var kubearmor = "kubearmor"
 
 var serviceAccountName = kubearmor
-var clusterRoleBindingName = "kubearmor-clusterrolebinding"
+var operatorServiceAccountName = "kubearmor-operator"
+var KubeArmorOperatorClusterRoleName = "kubearmor-operator-clusterrole"
+var KubeArmorOperatorManageClusterRoleName = "kubearmor-operator-manage-kubearmor-clusterrole"
+var KubeArmorOperatorManageControllerClusterRoleName = "kubearmor-operator-manage-controller-clusterrole"
+var KubeArmorClusterRoleName = "kubearmor-clusterrole"
+var KubeArmorControllerClusterRoleName = "kubearmor-controller-clusterrole"
+var KubeArmorSnitchClusterRoleName = "kubearmor-snitch"
+var KubeArmorControllerProxyClusterRoleName = "kubearmor-controller-proxy-role"
+
+var KubeArmorSnitchClusterroleBindingName = "kubearmor-snitch-binding"
+var KubeArmorControllerProxyClusterRoleBindingName = "kubearmor-controller-proxy-rolebinding"
+var KubeArmorControllerClusterRoleBindingName = "kubearmor-controller-clusterrolebinding"
+var KubeArmorClusterRoleBindingName = "kubearmor-clusterrolebinding"
+var KubeArmorOperatorManageControllerClusterRoleBindingName = "kubearmor-operator-manage-controller-clusterrole-binding"
+var KubeArmorOperatorManageClusterRoleBindingName = "kubearmor-operator-manage-kubearmor-clusterrole-binding"
+var KubeArmorOperatorClusterRoleBindingName = "kubearmor-operator-clusterrole-binding"
+
 var relayServiceName = kubearmor
 var relayDeploymentName = "kubearmor-relay"
 var policyManagerServiceName = "kubearmor-policy-manager-metrics-service"
