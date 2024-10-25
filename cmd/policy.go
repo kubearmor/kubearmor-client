2363553d653da1d1f3927a18938cdbc2af715c11
tesla59 <nishant@heim.id>
2024-10-25 15:24:29 +0000

    cmd(policy): merge get and list command in vm policy
    Signed-off-by: tesla59 <nishant@heim.id>

--- cmd/policy.go
+++ cmd/policy.go
@@ -82,32 +82,19 @@ var vmPolicyDeleteCmd = &cobra.Command{
 	},
 }
 
-var vmPolicyListCmd = &cobra.Command{
-	Use:   "list",
-	Short: "list policy for bare-metal vm/kvms control plane vm",
-	Long:  `list policy for bare-metal vm/kvms control plane vm`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		PolicyData, err := vm.GetPolicy(policyOptions)
-		if err != nil {
-			return err
-		}
-		armoredContainer, _ := utils.GetArmoredContainerData(PolicyData.ContainerList, PolicyData.ContainerMap)
-		policyOptions.PrintContainersSystemd(armoredContainer)
-		return nil
-	},
-}
-
 var vmPolicyGetCmd = &cobra.Command{
 	Use:   "get",
 	Short: "get policy for bare-metal vm/kvms control plane vm",
 	Long:  `get policy for bare-metal vm/kvms control plane vm`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) < 1 {
-			return errors.New("requires a container name as argument")
-		}
 		PolicyData, err := vm.GetPolicy(policyOptions)
 		if err != nil {
 			return err
+		}
+		if len(args) == 0 {
+			armoredContainer, _ := utils.GetArmoredContainerData(PolicyData.ContainerList, PolicyData.ContainerMap)
+			policyOptions.PrintContainersSystemd(armoredContainer)
+			return nil
 		}
 		if containerMap, ok := PolicyData.ContainerMap[args[0]]; ok {
 			for _, policy := range containerMap.PolicyDataList {
@@ -130,7 +117,6 @@ func init() {
 	// Subcommand for policy command
 	vmPolicyCmd.AddCommand(vmPolicyAddCmd)
 	vmPolicyCmd.AddCommand(vmPolicyDeleteCmd)
-	vmPolicyCmd.AddCommand(vmPolicyListCmd)
 	vmPolicyCmd.AddCommand(vmPolicyGetCmd)
 
 	// gRPC endpoint flag to communicate with KubeArmor. Available across subcommands.
