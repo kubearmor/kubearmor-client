fcd63af1daa12ce91fb2bd3e09adf4b0fc7ca1ba
mrrishi <mrrishi373@gmail.com>
2024-11-15 22:41:34 +0530

    fix: probe command
    Signed-off-by: mrrishi <mrrishi373@gmail.com>

--- recommend/registry/registry.go
+++ recommend/registry/registry.go
@@ -78,9 +78,6 @@ func (r *Scanner) loadDockerAuthConfigs() {
 		}
 
 		for _, conf := range confsWrapper.Auths {
-			if len(conf.Auth) == 0 {
-				continue
-			}
 			data, _ := base64.StdEncoding.DecodeString(conf.Auth)
 			userPass := strings.SplitN(string(data), ":", 2)
 			r.authConfiguration.authCreds = append(r.authConfiguration.authCreds, getAuthStr(userPass[0], userPass[1]))
