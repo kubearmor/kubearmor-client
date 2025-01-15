9cbd026a828e182dbf99917defc9e29fc3ab3329
rksharma95 <ramakant@accuknox.com>
2025-01-15 21:46:23 +0530

    remove deprecated package vm
    Signed-off-by: rksharma95 <ramakant@accuknox.com>

--- vm/policy.go
+++ vm/policy.go
@@ -4,18 +4,14 @@
 package vm
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
-	"net/http"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
-	"time"
 
-	kg "github.com/kubearmor/KubeArmor/KubeArmor/log"
 	tp "github.com/kubearmor/KubeArmor/KubeArmor/types"
 	pb "github.com/kubearmor/KubeArmor/protobuf"
 
@@ -77,40 +73,8 @@ func sendPolicyOverGRPC(o PolicyOptions, policyEventData []byte, kind string) er
 
 }
 
-func sendPolicyOverHTTP(address string, kind string, policyEventData []byte) error {
-
-	timeout := time.Duration(5 * time.Second)
-	client := http.Client{
-		Timeout: timeout,
-	}
-
-	var url string
-	if kind == KubeArmorHostPolicy {
-		url = address + "/policy/kubearmor"
-	}
-
-	request, err := http.NewRequest("POST", url, bytes.NewBuffer(policyEventData))
-	request.Header.Set("Content-type", "application/json")
-	if err != nil {
-		return fmt.Errorf("failed to send policy")
-	}
-
-	resp, err := client.Do(request)
-	if err != nil {
-		return fmt.Errorf("failed to send policy")
-	}
-	defer func() {
-		if err := resp.Body.Close(); err != nil {
-			kg.Warnf("Error closing http stream %s\n", err)
-		}
-	}()
-
-	fmt.Println("Success")
-	return nil
-}
-
 // PolicyHandling Function recives path to YAML file with the type of event and emits an Host Policy Event to KubeArmor gRPC/HTTP Server
-func PolicyHandling(t string, path string, o PolicyOptions, httpAddress string, isKvmsEnv bool) error {
+func PolicyHandling(t string, path string, o PolicyOptions) error {
 	var k struct {
 		Kind string `json:"kind"`
 	}
@@ -170,18 +134,12 @@ func PolicyHandling(t string, path string, o PolicyOptions, httpAddress string, 
 			return err
 		}
 
-		if isKvmsEnv {
-			// Non-K8s control plane with kvmservice, hence send policy over HTTP
-			if err = sendPolicyOverHTTP(httpAddress, k.Kind, policyEventData); err != nil {
-				return err
-			}
-		} else {
-			// Systemd mode, hence send policy over gRPC
-			if err = sendPolicyOverGRPC(o, policyEventData, k.Kind); err != nil {
-				return err
+		// Systemd mode, hence send policy over gRPC
+		if err = sendPolicyOverGRPC(o, policyEventData, k.Kind); err != nil {
+			return err
 
-			}
 		}
+
 	}
 
 	return nil
