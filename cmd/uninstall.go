2796204dd0b8398b3d8e9c8c2feff054bd187d56
Aryan-sharma11 <aryan1126.sharma@gmail.com>
2025-05-15 11:42:45 +0530

    feat: karmor install in unorchestrated env and vm
    Signed-off-by: Aryan-sharma11 <aryan1126.sharma@gmail.com>

--- cmd/uninstall.go
+++ cmd/uninstall.go
@@ -13,13 +13,18 @@ var uninstallOptions install.Options
 // uninstallCmd represents the get command
 var uninstallCmd = &cobra.Command{
 	Use:   "uninstall",
-	Short: "Uninstall KubeArmor from a Kubernetes Cluster",
-	Long:  `Uninstall KubeArmor from a Kubernetes Clusters`,
+	Short: "Uninstall KubeArmor",
+	Long:  `Uninstall KubeArmor  in either Kubernetes or non-Kubernetes mode.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if err := install.K8sUninstaller(client, uninstallOptions); err != nil {
-			if err := install.K8sLegacyUninstaller(client, uninstallOptions); err != nil {
-				return err
+		//check for systemd or docker installation
+		exist := install.CheckAndRemoveKAVmInstallation()
+		if !exist {
+			if err := install.K8sUninstaller(client, uninstallOptions); err != nil {
+				if err := install.K8sLegacyUninstaller(client, uninstallOptions); err != nil {
+					return err
+				}
 			}
+			return nil
 		}
 		return nil
 	},
@@ -31,4 +36,6 @@ func init() {
 	uninstallCmd.Flags().StringVarP(&uninstallOptions.Namespace, "namespace", "n", "", "If no namespace is specified, it defaults to all namespaces and deletes all KubeArmor objects across them.")
 	uninstallCmd.Flags().BoolVar(&uninstallOptions.Force, "force", false, "Force remove KubeArmor annotations from deployments. (Deployments might be restarted)")
 	uninstallCmd.Flags().BoolVar(&uninstallOptions.Verify, "verify", true, "Verify whether all KubeArmor resources are cleaned up or not")
+	uninstallCmd.Flags().BoolVar(&uninstallOptions.NonK8s, "nonk8s", false, "uninstall docker or systemd KubeArmor")
+
 }
