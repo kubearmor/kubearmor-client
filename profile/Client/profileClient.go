cc4e600bfad83a1ca514bb7b2059134464537b33
Harisudarsan <sudarshanhari561@gmail.com>
2025-05-27 11:01:34 +0530

    fix: karmor profile filtering logic
    Signed-off-by: Harisudarsan <sudarshanhari561@gmail.com>

--- profile/Client/profileClient.go
+++ profile/Client/profileClient.go
@@ -410,50 +410,57 @@ func generateRowsFromData(data []pb.Log, Operation string) []table.Row {
 	w := make(map[Profile]*Frequency)
 	for _, entry := range data {
 
-		if entry.Operation == Operation {
-			if (entry.NamespaceName == o1.Namespace) ||
-				(entry.PodName == o1.Pod) ||
-				(entry.ContainerName == o1.Container) ||
-				(len(o1.Namespace) == 0 && len(o1.Pod) == 0 && len(o1.Container) == 0) {
-				var p Profile
-				var logType string
-				if entry.Type == "HostLog" {
-					logType = "Host"
-					entry.NamespaceName = "--"
-					entry.ContainerName = "--"
-				} else {
-					logType = "Container"
-				}
+		if entry.Operation != Operation {
+			continue
+		}
 
-				if entry.Operation == "Syscall" {
-					p = Profile{
-						LogSource:     logType,
-						Namespace:     entry.NamespaceName,
-						ContainerName: entry.ContainerName,
-						Process:       entry.ProcessName,
-						Resource:      entry.Data,
-						Result:        entry.Result,
-					}
-				} else {
-					p = Profile{
-						LogSource:     logType,
-						Namespace:     entry.NamespaceName,
-						ContainerName: entry.ContainerName,
-						Process:       entry.ProcessName,
-						Resource:      entry.Resource,
-						Result:        entry.Result,
-					}
-				}
+		if (o1.Namespace != "") && (entry.NamespaceName != o1.Namespace) {
+			continue
+		}
+		if (o1.Pod != "") && (entry.PodName != o1.Pod) {
+			continue
+		}
+		if (o1.Container != "") && (entry.ContainerName != o1.Container) {
+			continue
+		}
 
-				f := &Frequency{
-					time: entry.UpdatedTime,
-				}
-				w[p] = f
-				m[p]++
-				w[p].freq = m[p]
+		var p Profile
+		var logType string
+		if entry.Type == "HostLog" {
+			logType = "Host"
+			entry.NamespaceName = "--"
+			entry.ContainerName = "--"
+		} else {
+			logType = "Container"
+		}
 
+		if entry.Operation == "Syscall" {
+			p = Profile{
+				LogSource:     logType,
+				Namespace:     entry.NamespaceName,
+				ContainerName: entry.ContainerName,
+				Process:       entry.ProcessName,
+				Resource:      entry.Data,
+				Result:        entry.Result,
+			}
+		} else {
+			p = Profile{
+				LogSource:     logType,
+				Namespace:     entry.NamespaceName,
+				ContainerName: entry.ContainerName,
+				Process:       entry.ProcessName,
+				Resource:      entry.Resource,
+				Result:        entry.Result,
 			}
 		}
+
+		f := &Frequency{
+			time: entry.UpdatedTime,
+		}
+		w[p] = f
+		m[p]++
+		w[p].freq = m[p]
+
 	}
 
 	finalmap := AggregateSummary(w, Operation)
